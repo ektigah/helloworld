a55c444c5f82fbc52c9e864952ea9381ea0bf81b
ektigah <ektigah@gmail.com>
2019-08-01 16:33:41 -0400

    added sass style gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'pages#index'  #"root to" in routes requires the hash tag and not the slash used by "get"
+  get 'pages/contact'
+  get 'pages/about' #go to the about action in the pages controller
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
